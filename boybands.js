7a7b97468edd1f6e874fa8bd274f5d0fc665be76
taylordotson <33382665+taylordotson@users.noreply.github.com>
2018-02-28 15:39:47 -0600

    added javascript and is working

--- boybands.js
+++ boybands.js
@@ -1,16 +1,24 @@
 let bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One Direction"];
 let vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
 
-var newBands = "";
-for (var i = 0; i < bands.length; i++) {
-    newBands += bands[i] + " ";
+// Get a reference to the appropriate DOM element for bands
+const bandElement = document.getElementById("boy-bands");
+
+// Get a reference to the appropriate DOM element for vegetables
+const veggieElement = document.getElementById("vegetables");
+
+// Execute a for loop that will iterate over the arrays
+for (let i = 0; i < 5; i += 1) {
+
+    // Get a reference to the current item in the bands array
+    const currentBand = bands[i] + "<br>";
+
+    // Update the innerHTML value of the DOM element for bands
+    currentBand += bandElement.innerHTML;
+
+    // Get a reference to the current item in the vegetables array
+    const currentVeggie = vegetables[i] + "<br>";
+
+    // Update the innerHTML value of the DOM element for vegetables
+    currentVeggie += veggieElement.innerHTML;
 }
-
-document.getElementById("boy-bands").innerHTML = newBands;
-
-var newVegs = "";
-for (var x = 0; x < vegetables.length; x++) {
-    newVegs += vegetables[x] + "/ ";
-}
-
-document.getElementById("vegetables").innerHTML = newVegs;
